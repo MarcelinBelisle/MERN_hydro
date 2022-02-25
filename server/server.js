6b7db1af8269c0f3bbb17ab625303b2d9c7c1659
Marcelin <marcelinbelisle@gmail.com>
2022-02-24 19:00:50 -0500

    public

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
+app.use(express.static('/public'));
+
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
