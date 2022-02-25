6586838db22dcaee563acedeedbbff5919f37588
Marcelin <marcelinbelisle@gmail.com>
2022-02-25 11:56:00 -0500

    port

--- server/server.js
+++ server/server.js
@@ -2,15 +2,17 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const cors = require("cors");
 const mongoose = require('mongoose')
 
-app.use(express.static(__dirname + '/public'));
+
 
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
+app.use(cors());
 app.use(express.json())
 
 const infoRouter = require('./routes/infos')
@@ -18,5 +20,7 @@ app.use('/infos', infoRouter)
 
 
 
-
-app.listen(process.env.PORT || 5000)
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}.`);
+  });
