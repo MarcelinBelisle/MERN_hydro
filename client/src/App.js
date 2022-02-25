3e302cd88de5797d42d886bc260515a4acd85088
Marcelin <marcelinbelisle@gmail.com>
2022-02-25 11:24:04 -0500

    construction

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import "./App.css";
+//IMPORTATION DES COMPONENTS
+import Tester from './components/Tester'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="background">
+    <Fragment>
+   <Tester/>
+   </Fragment>
+   </div>
   );
 }
 
---
+++ client/src/components/Tester.js
@@ -0,0 +1,17 @@
+import React, { Fragment, useEffect, useState } from "react";
+
+function Tester() {
+  return (
+    <Fragment>
+    <div >
+      <header>
+        <p>
+         lets go
+        </p>
+      </header>
+    </div>
+    </Fragment>
+  );
+}
+
+export default Tester;
